ea6530cdb0d3c6db04188f025c104ed7a34b2d66
Pavel Golenkov <pgolenkov@gmail.com>
2018-11-29 18:23:07 +0500

    Add Test class method for titles by category title

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,9 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :user_tests, dependent: :destroy
   belongs_to :category
+
+  def self.titles_by_category(title)
+    joins(:category).where(categories: { title: title })
+      .order(title: :desc).pluck(:title)
+  end
 end
