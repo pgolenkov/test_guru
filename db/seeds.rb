76b2c41277516285d581a47043cc16188294fa92
Pavel Golenkov <pgolenkov@gmail.com>
2018-11-29 18:00:40 +0500

    Fill seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+back, front, devops = %w[backend frontend devops].map { |title| Category.create(title: title) }
+tests = Test.create([{ title: 'Ruby', level: 0, category: back },
+                     { title: 'PHP', level: 2, category: back },
+                     { title: 'AngularJS', level: 3, category: front },
+                     { title: 'CSS', level: 2, category: front },
+                     { title: 'Linux', level: 1, category: devops }])
+
+questions = Question.create(
+  [
+    { body: 'Назовите метод, позволяющий добавить в массив новое значение', test: tests[0] },
+    { body: 'Какой вид типизации используется в языке Ruby?', test: tests[0] },
+    { body: 'Кто автор языка Ruby?', test: tests[0] },
+    { body: 'Какой сервис используется в Angular для работы с RESTful API?', test: tests[2] },
+    { body: 'Для каких целей используется селектор + ?', test: tests[3] }
+  ]
+)
+
+answers = Answer.create(
+  [
+    { body: 'pop', question: questions[0] },
+    { body: 'push', question: questions[0], correct: true },
+    { body: 'insert', question: questions[0], correct: true },
+
+    { body: 'Статическая типизация', question: questions[1] },
+    { body: 'Динамическая типизация', question: questions[1], correct: true },
+    { body: 'Строгая типизация', question: questions[1], correct: true },
+
+    { body: 'Matz', question: questions[2], correct: true },
+    { body: 'DHH', question: questions[2] },
+    { body: 'Google', question: questions[2] },
+
+    { body: '$scope', question: questions[3] },
+    { body: '$resource', question: questions[3], correct: true },
+    { body: '$http', question: questions[3], correct: true },
+
+    { body: 'Выбирает элемент, который находится непосредственно после указанного \
+элемента, если у них общий родитель', question: questions[4], correct: true },
+    { body: 'Выбирает элементы, которые являются дочерними непосредственно по \
+отношению к указанному элементу', question: questions[4] },
+    { body: 'Выбирает элементы, которые находятся после указанного элемента, \
+если у них общий родитель', question: questions[4] }
+  ]
+)
