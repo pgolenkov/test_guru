64e5210a953578bc35929fea34332c36f38c8699
Pavel Golenkov <pgolenkov@gmail.com>
2018-12-10 11:04:42 +0500

    Add owner association for tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_tests, dependent: :destroy
   has_many :tests, through: :user_tests
+  has_many :own_tests, class_name: 'Test', foreign_key: :owner_id
 
   def tests_by_level(level)
     tests.where(level: level)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_034427) do
+ActiveRecord::Schema.define(version: 2018_12_10_055402) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,7 +41,9 @@ ActiveRecord::Schema.define(version: 2018_11_30_034427) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "owner_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["owner_id"], name: "index_tests_on_owner_id"
   end
 
   create_table "user_tests", force: :cascade do |t|
