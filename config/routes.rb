836642307526cb1ab878968549c3b5be7bf05c41
Pavel Golenkov <pgolenkov@gmail.com>
2018-12-14 10:04:59 +0500

    Add task7 solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tests, only: :index do
+    resources :questions, shallow: true, except: [:edit, :update]
+  end
 end
