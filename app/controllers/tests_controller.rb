5c5f5072ab84db504812970f21cdb353c0eacab1
Pavel Golenkov <pgolenkov@gmail.com>
2018-12-16 17:59:12 +0500

    Add task8 solution

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,10 @@
 class TestsController < ApplicationController
   def index
-    @tests = Test.all
+    @tests = Test.select(:id, :title, 'COUNT(test_id) as count').joins(:questions).group(:test_id)
+  end
+
+  def show
+    @test = Test.find(params[:id])
+    redirect_to test_questions_path(@test)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
 end
