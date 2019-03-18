7215d54233fe93a4eb58f0585cd962d33e377330
Pavel Golenkov <pgolenkov@gmail.com>
2019-03-18 16:32:28 +0500

    Fix heroku app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'arcane-lowlands-22032.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'test-guru-pgolenkov.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
